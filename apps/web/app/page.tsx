1323d509e9247c663893e4561d0cc7c0aab5b56c
harsh <harshvaish2003@gmail.com>
2025-07-26 14:57:18 +0530

    update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
 import { prisma } from "@repo/db/client";
 
 export default async function Home() {
-  const user = await prisma.user.findFirst();
-  return <div>{user?.username}</div>;
+  const user = (await prisma.user.findFirst())?.username ?? "not found";
+  return <div>username: {user}</div>;
 }
